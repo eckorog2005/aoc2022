a9d6fa4b50237b6dcf6892d5291c3f2c3ba49d35
Roger Lamb <eckorog2005@gmail.com>
2022-12-11 00:58:41 -0500

    day 3

--- day3/day_3.py
+++ day3/day_3.py
@@ -1,2 +1,15 @@
-# file = open(".\day3\sample.txt")
-file = open(".\day3\input.txt")
+# file = open("./day3/sample.txt")
+file = open("./day3/input.txt")
+
+total = 0 
+for line in file:
+    first, last = line[:len(line)//2], line[len(line)//2:]
+    common_letters = list(set(first) & set(last))
+    for letter in common_letters:
+        asc_value = ord(letter)
+        if(asc_value >= 97):
+            total += (asc_value - 96)
+        else:
+            total += (asc_value - 38)
+
+print(total)
--- day3/day_3_part_2.py
+++ day3/day_3_part_2.py
@@ -1,2 +1,17 @@
-# file = open(".\day3\sample.txt")
-file = open(".\day3\input.txt")
+# file = open("./day3/sample.txt")
+file = open("./day3/input.txt")
+
+total = 0 
+lines = []
+for line in file:
+    lines.append(line.rstrip())
+    if len(lines) == 3:
+        common_letters = list(set(lines[0]) & set(lines[1]) & set(lines[2]))
+        for letter in common_letters:
+            asc_value = ord(letter)
+            if(asc_value >= 97):
+                total += (asc_value - 96)
+            else:
+                total += (asc_value - 38)
+        lines = []
+print(total)

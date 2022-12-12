9e123136a3cb81c54478d832b8da96c2c3a19b17
Roger Lamb <eckorog2005@gmail.com>
2022-12-11 21:12:38 -0500

    day 5

--- day5/day_5.py
+++ day5/day_5.py
@@ -1,3 +1,37 @@
-file = open(os.path.join(".","day1","sample.txt"))
 import os
-# file = open(os.path.join(".","day5","input.txt"))
+import re
+
+# file = open(os.path.join(".", "day5", "sample.txt"))
+file = open(os.path.join(".","day5","input.txt"))
+
+list_of_stacks = [[], [], [], [], [], [], [], [], []]
+
+for line in file:
+    counter = 0
+    if line == "\n":
+        break
+    while len(line) > 0:
+        if line.startswith(" "):
+            line = line[4:]
+            counter += 1
+        elif line.startswith("["):
+            temp = list_of_stacks[counter]
+            temp.append(line[1])
+            line = line[4:]
+            counter += 1
+
+for stack in list_of_stacks:
+    stack.reverse()
+
+for line in file:
+    temp = re.findall(r"\d+", line)
+    values = list(map(int, temp))
+    for i in range(values[0]):
+        list_of_stacks[values[2] - 1].append(list_of_stacks[values[1] - 1].pop())
+
+result = ""
+for stack in list_of_stacks:
+    if(len(stack) != 0):
+        result += stack.pop()
+
+print(result)
